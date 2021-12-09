898e1ddf3f3cc84d54c30f423ee15b136bc1e566
Carl G <griffiniiicarl@gmail.com>
2021-12-09 10:50:50 -0600

    deploy

--- main/db.js
+++ main/db.js
@@ -6,7 +6,8 @@ const isProduction = process.env.NODE_ENV === "production";
 const connectionString = `postgresql://${process.env.DB_USER}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL
+  connectionString: process.env.DATABASE_URL,
+  ssl: isProduction
 });
 
 module.exports = { pool };
